ac63c1972870dfafa757cb38c6df93e5d08e8d4c
Jonathan Tangpos <jtangpos@Jonathans-MacBook-Pro.local>
2020-07-09 15:53:58 +0800

    profiles page

--- src/app/pages/profiles/profiles.page.ts
+++ src/app/pages/profiles/profiles.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthConstants } from 'src/app/config/auth-constants';
+import { StorageService } from 'src/app/services/storage/storage.service';
 
 @Component({
   selector: 'app-profiles',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilesPage implements OnInit {
 
-  constructor() { }
+  user: any;
+
+  constructor(
+    private storageService: StorageService
+  ) { }
 
   ngOnInit() {
+    this.getUserData();
+  }
+
+  getUserData() {
+    this.storageService.get(AuthConstants.AUTH).then((res: any) => {
+      this.user = res;
+      console.log(this.user);
+    });
   }
 
 }
