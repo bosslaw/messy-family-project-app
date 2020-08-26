e492f2d8cdc0176b8ec015a9e890b5a663f407ac
Jonathan <jtangpos@wendt.enterprises>
2020-08-26 23:05:46 +0800

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.test/api/'
+  apiUrl: 'http://messyfamily.api/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://api.test/api/'
+  apiUrl: 'http://messyfamily.api/api/'
 };
 
 /*
