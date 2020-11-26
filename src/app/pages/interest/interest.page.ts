54696b42531c83c3f24ebbdd112e2ab99ed6f0e3
Jonathan <jtangpos@wendt.enterprises>
2020-11-26 09:31:02 +0800

    interest page

--- src/app/pages/interest/interest.page.ts
+++ src/app/pages/interest/interest.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
+import * as moment from 'moment';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { EventsService } from 'src/app/services/events/events.service';
+import { EventFormPage } from '../event-form/event-form.page';
 @Component({
   selector: 'app-interest',
   templateUrl: './interest.page.html',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterestPage implements OnInit {
 
-  constructor() { }
+  currentDay = new Date();
+  user: any;
+
+  constructor(
+    public modalCtrl: ModalController,
+    private authService: AuthService,
+    private eventService: EventsService
+    ) { }
 
   ngOnInit() {
+  }
+
+  async addEventModal() {
+    const modal = await this.modalCtrl.create({
+      component: EventFormPage,
+      componentProps: {
+        preselectedDate:this.currentDay
+      }
+    });
+
+    modal.onDidDismiss().then((event: any)=> {
+      if(event.data) {
+        this.addEvent(event.data);
+      }
+    });
+
+    return await modal.present();
+  }
+
+  addEvent(event) {
+
+    const formattedEvent = {
+      title: event.title,
+      start_date:  moment(event.start_date).format('YYYY-MM-DD hh:mm'),
+      end_date: moment(event.end_date).format('YYYY-MM-DD hh:mm'),
+      // allDay: event.data.allDay,
+      description: event.description,
+      location: event.location,
+      uid: this.user.Id,
+      status: 'Upcoming'
+    };
+
+    this.eventService.addEvent(formattedEvent).subscribe((res: any) => {
+      if(res.success) {
+        // this.loadLatestEvents();
+      }
+    },
+    (error: any) => {
+    });
+  }
+
+  ionViewWillEnter() {
+    this.authService.userData$.subscribe((res: any) => {
+      this.user = res;
+    })
   }
 
 }
